f736bdea751900e4df4902d8eaf789eff0d69f53
Howz <how.cht@gmail.com>
2017-12-07 14:25:46 +0800

    Initial commit

---
+++ Pages/Employee/Add.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HowzWebRazor001.Pages.Employee
+{
+    public class AddModel : PageModel
+    {
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            Message = "新增一筆員工資料";
+        }
+    }
+}
